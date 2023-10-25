4075ec7310f15fc3e75dcbc4e9cbfda36fd70cdb
Yagna123k <yagna82kb@gmail.com>
2023-10-25 21:21:43 +0530

    Completed js

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // Iteration 1: Making the play button in the index.html functional.
+var play = document.getElementById('play-button')
+
+play.addEventListener('click', ()=>{
+    window.location.href = 'game.html'
+})
 // Description: When the play button is clicked the game.html page should be opened
--- score.js
+++ score.js
@@ -1,1 +1,15 @@
 // Iteration 8: Making scoreboard functional
+var scoreBox = document.getElementById('score-board')
+
+let params = new URLSearchParams(window.location.search)
+
+let score = params.get("score")
+
+scoreBox.textContent = score
+
+var playagain = document.getElementById('play-again-button')
+
+playagain.addEventListener('click', ()=>{
+    window.location.href= 'game.html'
+})
+
